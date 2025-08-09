7a2cd7ec46cbc7dcad7b0b26008cf253d679b6bb
Sharath <sharathkumarraju1432@gmail.com>
2025-08-09 17:55:41 -0400

    Added Header

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, status, Header
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 
