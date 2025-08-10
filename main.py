159fc91053c5a49e1dd66aa8ae3be1df4d9d7ae0
Sharath <sharathkumarraju1432@gmail.com>
2025-08-10 11:44:02 -0400

    Change import

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, UploadFile, File, Form, Depends
+from fastapi import FastAPI, HTTPException, UploadFile, File, Form, Depends, Query
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse, JSONResponse
